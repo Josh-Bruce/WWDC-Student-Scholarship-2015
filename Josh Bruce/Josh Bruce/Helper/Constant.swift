2ff9c1abc46ccee28db8f03da16e6e57c7d487bf
Josh Bruce <josh@surftrack.co.uk>
2015-04-16 10:16:10 +0100

    Added a constant struct which will hold core colors and other key constants

---
+++ Josh Bruce/Josh Bruce/Helper/Constant.swift
@@ -0,0 +1,49 @@
+//
+//  Constant.swift
+//  Josh Bruce
+//
+//  Created by Josh Bruce on 16/04/2015.
+//  Copyright (c) 2015 Josh Bruce. All rights reserved.
+//
+
+import UIKit
+
+/**
+    A struct to hold all Constants for the application allowing for easy change and overview
+    or such colors and any other properties used throughout
+*/
+struct Constant {
+    
+    /**
+        Colors struct which holds the core colors used within the application
+    */
+    struct Colors {
+        
+        /**
+            Pink Color
+        */
+        static let PinkColor = UIColor(red: 223.0/255.0, green: 62.0/255.0, blue: 147.0/255.0, alpha: 1.0)
+        
+        /**
+            Orange Color
+        */
+        static let OrangeColor = UIColor(red: 233.0/255.0, green: 128.0/255.0, blue: 112.0/255.0, alpha: 1.0)
+        
+        /**
+            Yellow Color
+        */
+        static let YellowColor = UIColor(red: 243.0/255.0, green: 191.0/255.0, blue: 79.0/255.0, alpha: 1.0)
+        
+        /**
+            Blue Color
+        */
+        static let BlueColor = UIColor(red: 62.0/255.0, green: 68.0/255.0, blue: 197.0/255.0, alpha: 1.0)
+        
+        /**
+            Green Color
+        */
+        static let GreenColor = UIColor(red: 111.0/255.0, green: 220.0/255.0, blue: 172.0/255.0, alpha: 1.0)
+        
+    }
+    
+}
