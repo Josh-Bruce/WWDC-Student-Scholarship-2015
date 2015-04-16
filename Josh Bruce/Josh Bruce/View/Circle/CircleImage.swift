81f88ae5a3ae67ab66e9e168bed4df0485b4fdf5
Josh Bruce <josh@surftrack.co.uk>
2015-04-16 12:18:18 +0100

    Playing with storyboard layout and added a UIImageView subclass.

---
+++ Josh Bruce/Josh Bruce/View/Circle/CircleImage.swift
@@ -0,0 +1,43 @@
+//
+//  CircleImage.swift
+//  Josh Bruce
+//
+//  Created by Josh Bruce on 16/04/2015.
+//  Copyright (c) 2015 Josh Bruce. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+@IBDesignable class CircleImage: UIImageView {
+    
+    // MARK: Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        drawCircle()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // MARK: Drawing
+    
+    override func drawRect(rect: CGRect) {
+        super.drawRect(rect)
+        
+        drawCircle()
+    }
+    
+    // MARK: Methods
+    
+    func drawCircle() {
+        // Draw a circle based upon width / 2
+        layer.cornerRadius = frame.size.width / 2
+        layer.masksToBounds = true
+        clipsToBounds = true
+    }
+   
+}
