2d75eb6ae7136a861e810830678106c4352d6034
Josh Bruce <josh@surftrack.co.uk>
2015-04-17 12:24:49 +0100

    Created a welcome controller for introduction.

---
+++ Josh Bruce/Josh Bruce/Controller/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  Josh Bruce
+//
+//  Created by Josh Bruce on 17/04/2015.
+//  Copyright (c) 2015 Josh Bruce. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: BaseViewController {
+    
+    // MARK: - Outlets
+    
+    // MARK: - Lifecycle
+   
+}
