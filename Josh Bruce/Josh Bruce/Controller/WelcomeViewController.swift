2b0087de8492a3c4247f70d4f29fc6bf3065765e
Josh Bruce <josh@surftrack.co.uk>
2015-04-18 12:23:24 +0100

    Basic animation of welcome with auto layout.

--- Josh Bruce/Josh Bruce/Controller/WelcomeViewController.swift
+++ Josh Bruce/Josh Bruce/Controller/WelcomeViewController.swift
@@ -12,10 +12,53 @@ class WelcomeViewController: BaseViewController {
     
     // MARK: - Outlets
     
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var welcomeLabelCenterYConstraint: NSLayoutConstraint!
+
+    // MARK: - Properties
+    
+    var welcomeLabelEndConstraints: [AnyObject]?
+    
+    var holder: [AnyObject]?
+    
     // MARK: - Lifecycle
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        welcomeLabelCenterYConstraint.constant = view.frame.height
+        view.layoutIfNeeded()
+        
+        UIView.animateWithDuration(1.0, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 2.0, options: UIViewAnimationOptions.BeginFromCurrentState, animations: { () -> Void in
+            self.welcomeLabelCenterYConstraint.constant = 0
+            self.welcomeLabel.alpha = 1
+            self.view.layoutIfNeeded()
+        }) { (success) -> Void in
+
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Get end constraints for views
+//        getEndConstraintsForViews()
+//        
+//        UIView.animateWithDuration(2.0, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 10, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+//            self.view.addConstraints(self.holder!)
+//            println("")
+//        }) { (success) -> Void in
+//            
+//        }
+    }
+    
+    // MARK: - Methods
+    
+    func getEndConstraintsForViews() {
+        // Extract the ending constraints
+        welcomeLabelEndConstraints = welcomeLabel.constraints()
+        holder = view.constraints()
+        view.removeConstraints(holder!)
     }
    
 }
